3b21c1eb8fc0e7c8a550104f1d57f1a5de1fbdf0
clifford <clifford@lautus.net>
2012-03-22 16:07:11 +0200

    CG: refactored script

--- wheres-my-commit.rb
+++ wheres-my-commit.rb
@@ -1,18 +1,33 @@
 #!/usr/bin/env ruby 
 
-unless ARGV.size == 1
-  puts "usage: #{__FILE__} <project name>"
+unless ARGV.size == 2
+  puts "usage: #{__FILE__} <project name> <branch name>"
+  puts "example1: #{__FILE__} gps remotes/origin/release-gls-2.14"
+  puts "example2: #{__FILE__} source/gls-rest-api-tests remotes/origin/release-gls-2.14"
   exit
 end
 
 project = ARGV[0] 
-cwd = `pwd`
-Dir.chdir '/home/r2d2/gps'
-result = `git log --pretty=oneline --since="2 week ago"`
-sha_map = {}
-commits = result.split("\n").map do |commit| 
-  /(?<sha>^\w+(?=\s?)) (?<msg>.*$)/=~ commit
-  sha_map[sha] = msg 
+source_branch = ARGV[1] 
+
+def run_in_dir(dir)
+  cwd = Dir.pwd
+  Dir.chdir dir
+  return yield if block_given?
+  Dir.chdir cwd
+end
+
+def recent_commits(branch)
+  `git fetch && git log --pretty=oneline --since="2 week ago" #{branch}`
+end
+
+def find_recent_commits(branch)
+  sha_map = {}
+  commits = recent_commits(branch).split("\n").map do |commit| 
+    /(?<sha>^\w+(?=\s?)) (?<msg>.*$)/=~ commit
+    sha_map[sha] = msg 
+  end
+  sha_map
 end
 
 def branches_containing(sha)
@@ -24,10 +39,17 @@ def branches_containing(sha)
   branch_names
 end
 
+def find_branches_for_recent_commits(project, branch)
+  dir = "/home/r2d2/#{project}"
+  recent_commits = run_in_dir(dir) {find_recent_commits(branch)}
+  recent_commits.each do |sha, msg|
+    branches_including_sha = run_in_dir(dir) { branches_containing(sha) }
+    printf "\n%s : %s %s\n"%[sha, msg, format_branches(branches_including_sha)]
+  end
+end
+
 def format_branches(branches)
   printf "\t%s\n"%[branches.join("\n\t")]
 end
 
-sha_map.each do |sha, msg|
-  printf "\n%s : %s %s\n"%[sha, msg, format_branches(branches_containing(sha))]
-end
+find_branches_for_recent_commits(project, source_branch)
